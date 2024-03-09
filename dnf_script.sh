768a846aa737fb75e566a7956c560d27db6f2f36
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-03-09 03:41:29 -0500

    Create dnf_script.sh

---
+++ dnf_script.sh
@@ -0,0 +1,7 @@
+dnf update
+dnf upgrade
+dnf config-manager --set-enabled crb
+dnf install epel-release epel-next-release
+dnf update
+dnf install podman vim buildah gcc
+dnf install bcc-devel bcc-tools git libseccomp-devel golang-github-cpuguy83-md2man make curl
