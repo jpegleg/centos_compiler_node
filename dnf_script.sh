9ab768813e7e85b751b189820d379ff7e5a61c2c
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-03-09 03:49:19 -0500

    Update dnf_script.sh

--- dnf_script.sh
+++ dnf_script.sh
@@ -3,5 +3,5 @@ dnf upgrade
 dnf config-manager --set-enabled crb
 dnf install epel-release epel-next-release
 dnf update
-dnf install podman vim buildah gcc
+dnf install podman vim buildah gcc clang
 dnf install bcc-devel bcc-tools git libseccomp-devel golang-github-cpuguy83-md2man make curl
