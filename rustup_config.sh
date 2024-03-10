2ab777fc3aeb1b175d474e711eacf6c72173e286
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-03-10 00:15:40 -0500

    Create rustup_config.sh

---
+++ rustup_config.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+# this needs to be run for the user doing the builds, potentially multiple users depending on the design
+# and does not need super user access, just internet networking and writing within ~ using compilers.
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
+~/.cargo/bin/cargo install clippy
+~/.cargo/bin/cargo install rage
+~/.cargo/bin/cargo install cross
+~/.cargo/bin/cross --version
+~/.cargo/bin/cargo --version
+~/.cargo/bin/rage --version
