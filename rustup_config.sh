669164dd4be034bb131c52572e1ad8dbacfc06a4
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-04-05 19:33:20 -0400

    Update rustup_config.sh

--- rustup_config.sh
+++ rustup_config.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 # this needs to be run for the user doing the builds, potentially multiple users depending on the design
 # and does not need super user access, just internet networking and writing within ~ using compilers.
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y &&
+echo "installed cargo with rustup." &&
+echo "installing clippy, rage, and cross then check versions of them..." &&
 ~/.cargo/bin/cargo install clippy
 ~/.cargo/bin/cargo install rage
 ~/.cargo/bin/cargo install cross
